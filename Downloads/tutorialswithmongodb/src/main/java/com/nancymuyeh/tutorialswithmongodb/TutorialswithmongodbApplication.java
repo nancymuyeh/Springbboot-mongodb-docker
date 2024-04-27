8162e93385aa7b1b977ce8cd560e894953dcf383
nancymuyeh <nancymuyeh@gmail.com>
2024-04-27 04:27:01 +0100

    initial commit

---
+++ Downloads/tutorialswithmongodb/src/main/java/com/nancymuyeh/tutorialswithmongodb/TutorialswithmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.nancymuyeh.tutorialswithmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialswithmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialswithmongodbApplication.class, args);
+	}
+
+}
