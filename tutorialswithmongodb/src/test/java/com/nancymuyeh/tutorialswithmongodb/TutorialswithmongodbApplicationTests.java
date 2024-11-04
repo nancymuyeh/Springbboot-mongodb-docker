e28c484a71bb76f6e68ce04f5a92b09cb28778eb
Nancy Muyeh <nancymuyeh@gmail.com>
2024-11-04 10:19:58 +0100

    refactor project structure

---
+++ tutorialswithmongodb/src/test/java/com/nancymuyeh/tutorialswithmongodb/TutorialswithmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nancymuyeh.tutorialswithmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialswithmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
